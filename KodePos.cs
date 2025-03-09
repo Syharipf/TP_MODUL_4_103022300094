c28a9a7727409331f6242f85452f81aa1f4e1b83
Syarif <154748261+Syharipf@users.noreply.github.com>
2025-03-09 17:07:28 +0700

    Menambahkan implementasi dengan table-driven

---
+++ KodePos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class KodePos
+{
+    // Array dua dimensi untuk menyimpan kelurahan dan kode pos
+    private string[,] daftarKodePos = {
+        {"Batununggal", "40266"},
+        {"Kujangsari", "40287"},
+        {"Mengger", "40267"},
+        {"Wates", "40256"},
+        {"Cijaura", "40287"},
+        {"Jatisari", "40286"},
+        {"Margasari", "40286"},
+        {"Sekejati", "40286"},
+        {"Kebonwaru", "40272"},
+        {"Maleer", "40274"},
+        {"Samoja", "40273"}
+    };
+
+    // Method untuk mendapatkan kode pos berdasarkan nama kelurahan
+    public string GetKodePos(string kelurahan)
+    {
+        for (int i = 0; i < daftarKodePos.GetLength(0); i++) // Iterasi semua baris
+        {
+            if (daftarKodePos[i, 0].Equals(kelurahan, StringComparison.OrdinalIgnoreCase))
+            {
+                return daftarKodePos[i, 1]; // Mengembalikan kode pos
+            }
+        }
+        return "Kelurahan tidak ditemukan!";
+    }
+}
--- obj/Debug/net8.0/TP_MODUL_4_103022300094.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP_MODUL_4_103022300094.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL_4_103022300094")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcea2cc171b7212ef83887b0be52ca62c6f3dcf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL_4_103022300094")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL_4_103022300094")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
